8813dca3c1476617b35fe3007a8678c23ac322af
VadimBaraa <rentsvadim@gmail.com>
2025-04-15 10:51:06 +0300

    .

--- EventGenerator/obj/Debug/net9.0/EventGenerator.AssemblyInfo.cs
+++ EventGenerator/obj/Debug/net9.0/EventGenerator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca110da06bbfa50d2e1dc88895df6e28f8c7dca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EventProcessor/obj/Debug/net9.0/EventProcessor.AssemblyInfo.cs
+++ EventProcessor/obj/Debug/net9.0/EventProcessor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventProcessor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca110da06bbfa50d2e1dc88895df6e28f8c7dca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventProcessor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventProcessor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
